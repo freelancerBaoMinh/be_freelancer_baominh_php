884c5b624318d5d3a48017a29fb1b3c501b92414
backend265 <backend2605@gmail.com>
2021-12-30 17:54:16 +0700

    management package, user, contract

--- database/migrations/2021_12_24_022958_create_contracts_table.php
+++ database/migrations/2021_12_24_022958_create_contracts_table.php
@@ -26,7 +26,7 @@ class CreateContractsTable extends Migration
             $table->timestamp('effective_date')->nullable();
             $table->timestamp('end_date')->nullable();
             $table->integer('package_code')->comment('package code id')->index();
-            $table->string('email')->index();
+            $table->string('email', 50)->index();
             $table->integer('user_id')->default(0);
             $table->string('relationship');
             $table->boolean('status')->default(1)->comment('0: delete, 1: normal');
